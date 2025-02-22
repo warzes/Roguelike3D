6c05dd5dfac0956ef581ac839f690454986a91e9
warzes <warzes@mail.ru>
2025-02-22 18:24:57 +1000

    - очистка

--- src/Game/main.cpp
+++ src/Game/main.cpp
@@ -6,34 +6,10 @@
 #	pragma comment( lib, "Engine.lib" )
 #endif
 //=============================================================================
-
-#include <iostream>
-
-struct Tesss
-{
-	Tesss() { std::cout << "Tessss"; }
-	Tesss(const Tesss&) { std::cout << "Tessss(&)"; }
-	Tesss(Tesss&&) { std::cout << "Tessss(&&)"; }
-	~Tesss() { std::cout << "~Tessss"; }
-};
-
-
-std::expected<Tesss, std::string> Foo()
-{
-	//return {};
-
-	return std::unexpected("dfd");
-}
-
 int main(
 	[[maybe_unused]] int   argc,
 	[[maybe_unused]] char* argv[])
 {
-	//auto foo = Foo();
-	//if (!foo.has_value())
-	//{
-	//	std::cout << foo.error();
-	//}
 	GameApp app;
 	app.Run();
 }
