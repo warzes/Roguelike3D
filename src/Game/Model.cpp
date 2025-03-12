f34f65cb68b8040f8f23acf6f84452e05017de8f
warzes <warzes@mail.ru>
2025-03-12 21:25:35 +1000

    - ссылки

--- src/Game/Model.cpp
+++ src/Game/Model.cpp
@@ -1,2 +1,2 @@
 ﻿#include "stdafx.h"
-#include "Mesh.h"
+#include "Model.h"
---
+++ src/Game/Model.h
@@ -0,0 +1,25 @@
+﻿#pragma once
+
+#include "Vertex.h" 
+
+https://github.com/syoyo/tinygltf
+https://github.com/okhmanyuk-ev/skygfx/blob/master/src/skygfx/other.h
+https://github.com/nustxujun/Portable -!!!
+
+
+https://github.com/fisherman1504/d3d11_project
+
+
+
+
+https://github.com/matt77hias/Hieroglyph-v3/tree/master/Source
+
+https://github.com/arnkov/poki
+
+
+
+
+
+
+
+https://github.com/benikabocha/saba - mmd
